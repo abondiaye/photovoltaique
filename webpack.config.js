330b524705b1775adc09e8dbd36b761015b4af12
abondiaye <a_ndiaye@outlook.com>
2025-08-29 13:52:31 +0200

    add:calendrier affichage

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,38 @@
 // webpack.config.js
 const Encore = require('@symfony/webpack-encore');
 
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
 Encore
-    .setOutputPath('public/build/')
-    .setPublicPath('/build')
-    .addEntry('app', './assets/app.js') 
-    .addEntry('bg', './assets/background.js')
-    .enableSingleRuntimeChunk()
-    .splitEntryChunks()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
-    .enableSassLoader();
-    
+  .setOutputPath('public/build/')
+  .setPublicPath('/build')
 
-const config = Encore.getWebpackConfig();
-config.mode = Encore.isProduction() ? 'production' : 'development';
+  // Entrées
+  .addEntry('app', './assets/app.js')
+  .addEntry('bg', './assets/background.js') // si tu utilises le canvas animé
 
-module.exports = config;
+  // Optimisations
+  .splitEntryChunks()
+  .enableSingleRuntimeChunk()
+
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+
+  // CSS/SCSS + PostCSS
+  .enableSassLoader()
+  .enablePostCssLoader()
+
+  // Babel (ES compat + polyfills core-js)
+  .configureBabel((config) => {
+    config.presets.push([
+      '@babel/preset-env',
+      { useBuiltIns: 'usage', corejs: 3 }
+    ]);
+  })
+;
+
+module.exports = Encore.getWebpackConfig();
